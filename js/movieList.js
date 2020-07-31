e39648d0592d4e51b6dc9f0ef7ba19e0f172c0a3
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-31 03:49:19 +0100

    movie-list

--- js/movieList.js
+++ js/movieList.js
@@ -7,13 +7,10 @@ const movie_list = [
     Runtime: "162 min",
     Genre: "Action, Adventure, Fantasy",
     Director: "James Cameron",
-    Writer: "James Cameron",
     Language: "English, Spanish",
     Country: "USA, UK",
-    Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
     Poster:
       "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
-    Metascore: "83",
     Response: "True",
   },
   {
@@ -24,29 +21,11 @@ const movie_list = [
     Runtime: "101 min",
     Genre: "Drama, Horror, Sci-Fi",
     Director: "Francis Lawrence",
-    Writer:
-      "Mark Protosevich (screenplay), Akiva Goldsman (screenplay), Richard Matheson (novel), John William Corrington, Joyce Hooper Corrington",
-    Actors:
-      "Will Smith, Alice Braga, Charlie Tahan, Salli Richardson-Whitfield",
-    Plot:
-      "Years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.",
     Language: "English",
     Country: "USA",
-    Awards: "9 wins & 21 nominations.",
     Poster:
       "http://ia.media-imdb.com/images/M/MV5BMTU4NzMyNDk1OV5BMl5BanBnXkFtZTcwOTEwMzU1MQ@@._V1_SX300.jpg",
-    Metascore: "65",
-    imdbRating: "7.2",
-    imdbVotes: "533,874",
-    imdbID: "tt0480249",
-    Type: "movie",
     Response: "True",
-    Images: [
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI0NTI4NjE3NV5BMl5BanBnXkFtZTYwMDA0Nzc4._V1_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwMDg2MDU4M15BMl5BanBnXkFtZTYwMTA0Nzc4._V1_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5MDM1OTU5OV5BMl5BanBnXkFtZTYwMjA0Nzc4._V1_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0MTI2NjMzMzFeQTJeQWpwZ15BbWU2MDMwNDc3OA@@._V1_.jpg",
-    ],
   },
   {
     Title: "300",
@@ -56,27 +35,11 @@ const movie_list = [
     Runtime: "117 min",
     Genre: "Action, Drama, Fantasy",
     Director: "Zack Snyder",
-    Writer:
-      "Zack Snyder (screenplay), Kurt Johnstad (screenplay), Michael Gordon (screenplay), Frank Miller (graphic novel), Lynn Varley (graphic novel)",
-    Actors: "Gerard Butler, Lena Headey, Dominic West, David Wenham",
-    Plot:
-      "King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 B.C.",
     Language: "English",
     Country: "USA",
-    Awards: "16 wins & 42 nominations.",
     Poster:
       "http://ia.media-imdb.com/images/M/MV5BMjAzNTkzNjcxNl5BMl5BanBnXkFtZTYwNDA4NjE3._V1_SX300.jpg",
-    Metascore: "52",
-    imdbRating: "7.7",
-    imdbVotes: "611,046",
-    imdbID: "tt0416449",
-    Type: "movie",
     Response: "True",
-    Images: [
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMwNTg5MzMwMV5BMl5BanBnXkFtZTcwMzA2NTIyMw@@._V1_SX1777_CR0,0,1777,937_AL_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQwNTgyNTMzNF5BMl5BanBnXkFtZTcwNDA2NTIyMw@@._V1_SX1777_CR0,0,1777,935_AL_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0MjQzOTEwMV5BMl5BanBnXkFtZTcwMzE2NTIyMw@@._V1_SX1777_CR0,0,1777,947_AL_.jpg",
-    ],
   },
   {
     Title: "The Avengers",
