78eaaf1b78f26a111b69fb74682f9bfee37f5c7c
Davies Esiro <ojurereoluwad@gmail.com>
2020-08-01 22:41:57 +0100

    done

--- js/movieClass.js
+++ js/movieClass.js
@@ -1,13 +1,25 @@
-class movieClass{
-    constructor(){
+class MovieClass {
+    constructor(movie_list) {
         this.movies = movie_list
     }
 
-    getAllMovies(){
+    getAllMovies() {
         return this.movies;
     }
-    searchByString(string){
-        let movie_list = this.movies;
-        let newMovieArray = [];
+
+    searchByString(string) {
+        // Google about filter
+        // we are searching by title 
+        return this.movies.filter((movie) => movie.Title.toLowerCase().includes(string))
+    }
+
+    rentMovie(id) {
+        //First find the movie you want to rent
+        const movieIndex = this.movies.findIndex(movie => movie.id === id);
+        //splice removes an element from from a array and then returns an array the removed elements in this case
+        //we want to remove just one element, so it will be at position 0
+
+        //try this console.log(this.movies.splice(movieIndex, 1))
+        return this.movies.splice(movieIndex, 1)[0]
     }
 }
