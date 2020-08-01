f3825cff1b0aff49fc8877fc7d2034e4e3cb5f6e
Adedipupo <bayonleolaluwa@gmail.com>
2020-08-01 23:08:19 +0100

    wip

--- js/index.js
+++ js/index.js
@@ -7,10 +7,9 @@ const renderMovies = (movies) => {
     $root.innerHTML = ''; // clearing all the elements in the root
 
     movies.forEach((movie) => {
-        // you can reseach literal strings
         const cardInnerElements =
             `
-                <img class='card__image' src='${movie.Images[0]}/>
+                <img class='card__image' src='${movie.Poster[0]}/>
                 <h3 class='card__title'>Title: ${movie.Title}</h3>
                 <div class='card__meta'>
                     <p>Year: ${movie.Year} </p>
@@ -31,8 +30,6 @@ const renderMovies = (movies) => {
     });
 }
 
-// this will make sure the cards are displayed
-// you can comment it to see the differnce 
 renderMovies(Movie.getAllMovies())
 
 const $input = document.getElementById('myInput');
@@ -40,24 +37,17 @@ const $input = document.getElementById('myInput');
 $input.addEventListener('keyup', (e) => {
     const searchField = e.target.value;
 
-    //if the input is empty we want to display all the movies
     if (searchField === '') {
         return renderMovies(Movie.getAllMovies())
     }
-
-    //go to the movieclass to see what this does
     const searchMovies = Movie.searchByString(searchField);
 
-    //render the movies that match the searchstring
     renderMovies(searchMovies)
 });
 
-// this will be called when the rent button is click
-// look at the card__meta button 
 const rentMovie = (id) => {
     const rentedMovie = Movie.rentMovie(id)
 
     alert(`You have rented ${rentedMovie.Title}`);
-    // this will render the remaining movies
     renderMovies(Movie.getAllMovies())
 }
